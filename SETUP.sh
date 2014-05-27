dd2d57232355f7046044cd2b5414a79fccbe43cf
Craig Buchek <craig@boochtek.com>
2014-05-27 14:02:51 -0500

    Update SETUP script to install MacTex.

--- SETUP.sh
+++ SETUP.sh
@@ -2,3 +2,6 @@
 
 # Install Pandoc.
 brew cask install pandoc
+
+# Install MacTeX. We need its pdflatex command to generate PDFs. NOTE: This takes a LONG time to download all 2.3 GB.
+brew cask install mactex
