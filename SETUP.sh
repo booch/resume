8dcc454e478127ae00625f6f858541745a0cc8da
Craig Buchek <craig@boochtek.com>
2021-01-03 17:44:51 -0800

    SETUP.sh - actually install the programs we need

--- SETUP.sh
+++ SETUP.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# TODO: Install wkhtmltopdf (from binary installer).
-# TODO: Install kramdown gem (including binary).
+# Install Kramdown gem (including binary).
+hash kramdown 2>/dev/null || gem install kramdown
+
+# Install wkhtmltopdf.
+hash wkhtmltopdf 2>/dev/null || brew install wkhtmltopdf
+
+# Install HTML5 version of Tidy.
+[[ $(type tidy) == 'tidy is /usr/local/bin/tidy' ]] || echo brew install tidy-html5
+
+# UNIX convention is to succeed silently, but that seems a little too surprising for a setup script.
+echo "All prerequisites installed."
